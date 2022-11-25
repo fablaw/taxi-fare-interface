40fc3c18d6463938922682cc51bfe4595861ae71
Fabian Law <114812168+fablaw@users.noreply.github.com>
2022-11-25 10:21:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+const taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmFibGF3IiwiYSI6ImNsYXdha3BnODBjYnQzcHA0bDJmMmx2ejgifQ.RNyYESU5HY66wCUOyDxDPA';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
